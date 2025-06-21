bd2985faa44880dec133249f92eefbd8f4cc8269
Safat11 <safat3622@gmail.com>
2025-06-21 23:18:28 +0600

    Create Delete API And Delete A Server Booking

--- index.js
+++ index.js
@@ -51,22 +51,34 @@ async function run() {
         })
 
         // bookings
-        app.get('/bookings', async(req, res) => {
+        app.get('/bookings', async (req, res) => {
             console.log(req.query.email);
             let query = {};
-            if(req.query?.email){
-                query = {email: req.query.email}
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await bookingCollection.find().toArray();
             res.send(result);
         })
 
-        app.post('/bookings', async(req, res) => {
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
         });
+
+        app.put('/bookings/:id', async (req, res) => {
+            const updatedBooking = req.body;
+
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
