e616147a0770a6841cbdc4bd69bb2e6d09077a3d
Safat11 <safat3622@gmail.com>
2025-06-25 23:59:22 +0600

    Create JWT token on server, Get JWT token on client side

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings')
 
+        // jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+            console.log(token);
+            res.send({token});
+        })
+
+        // services routes
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -51,7 +62,7 @@ async function run() {
             res.send(result);
         })
 
-        // bookings
+        // bookings routes
         app.get('/bookings', async (req, res) => {
             console.log(req.query.email);
             let query = {};
